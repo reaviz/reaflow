67fdb29f3725f8f97fe675f1663e499a840682e8
Austin <amcdaniel2@gmail.com>
2020-10-28 15:09:06 -0400

    moar

--- src/Canvas.stories.tsx
+++ src/Canvas.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Canvas } from './Canvas';
 
-export const Simple = () => <Canvas />;
+export const Simple = () => <Canvas nodes={[]} edges={[]} />;
 
 export default {
   title: 'Canvas',
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,5 +1,67 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
+import { useId } from 'rdk';
+import useDimensions from 'react-cool-dimensions';
 
-export const Canvas: FC<any> = () => {
-  return null;
+export interface Node<T = any> {
+  id: string;
+  parent?: Node;
+  edges?: Edge[];
+  ports?: Port[];
+  data?: T;
+}
+
+export interface Edge<T = any> {
+  id: string;
+  from?: Node | Port;
+  to?: Node | Port;
+  data?: T;
+}
+
+export interface Port {
+  id: string;
+}
+
+export interface EditorCanvasProps {
+  nodes: Node[];
+  edges: Edge[];
+  layout?: 'elk' | 'manual';
+  id?: string;
+  className?: string;
+  minZoom?: number;
+  maxZoom?: number;
+  zoomStep?: number;
+  disabled?: boolean;
+  height?: number;
+  width?: number;
+  maxHeight?: number;
+  maxWidth?: number;
+}
+
+export const Canvas: FC<EditorCanvasProps> = ({
+  id,
+  className,
+  layout = 'manual',
+  height = '100%',
+  width = '100%',
+  maxHeight = 2000,
+  maxWidth = 2000
+}) => {
+  const genId = useId(id);
+  const { ref, width: svgWidth, height: svgHeight } = useDimensions<HTMLDivElement>();
+
+  return (
+    <div ref={ref} style={{ height, width }}>
+      <svg
+        id={genId}
+        className={className}
+        height={svgHeight}
+        width={svgWidth}
+        style={{ height: maxHeight, width: maxWidth }}
+      >
+        {height > 0 && width > 0 && (
+          <g></g>
+        )}
+      </svg>
+    </div>
+  );
 };
