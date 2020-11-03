2467d8a6a0974e415aaed67b6222c991b0fea832
Austin <amcdaniel2@gmail.com>
2020-11-03 14:15:50 -0500

    improve stories

--- src/Canvas.stories.tsx
+++ src/Canvas.stories.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import { Canvas } from './Canvas';
 
-const nodes = [
+const simpleNodes = [
+  {
+    id: '1'
+  },
+  {
+    id: '2'
+  }
+];
+
+export const Simple = () => (
+  <div style={{ border: 'solid 1px blue', height: 500, width: 500 }}>
+    <Canvas
+      nodes={simpleNodes}
+      edges={[
+        {
+          id: '1-2',
+          from: simpleNodes[0],
+          to: simpleNodes[1]
+        }
+      ]}
+    />
+  </div>
+);
+
+const joinNodes = [
   {
     id: '1'
   },
@@ -10,23 +34,36 @@ const nodes = [
   },
   {
     id: '3'
+  },
+  {
+    id: '4'
   }
 ];
 
-export const Simple = () => (
+export const Joins = () => (
   <div style={{ border: 'solid 1px blue', height: 500, width: 500 }}>
     <Canvas
-      nodes={nodes}
+      nodes={joinNodes}
       edges={[
         {
           id: '1-2',
-          from: nodes[0],
-          to: nodes[1]
+          from: joinNodes[0],
+          to: joinNodes[1]
         },
         {
           id: '1-3',
-          from: nodes[0],
-          to: nodes[2]
+          from: joinNodes[0],
+          to: joinNodes[2]
+        },
+        {
+          id: '2-3',
+          from: joinNodes[2],
+          to: joinNodes[3]
+        },
+        {
+          id: '2-4',
+          from: joinNodes[1],
+          to: joinNodes[3]
         }
       ]}
     />
