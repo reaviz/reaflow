fa7de9dae6e27e3f9e451f8017fe3b56994cfdf1
Austin <amcdaniel2@gmail.com>
2020-11-06 15:18:16 -0500

    thresholds

--- src/utils/useNodeDrag.ts
+++ src/utils/useNodeDrag.ts
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { useDrag } from 'react-use-gesture';
 import { State } from 'react-use-gesture/dist/types';
 import { NodeData } from '../types';
@@ -37,12 +38,21 @@ export interface NodeDragProps extends NodeDragEvents {
 
 export const useNodeDrag = ({ x, y, height, width, onDrag, onDragEnd, onDragStart, node, disabled }: NodeDragProps) => {
   const initial: Position = [width / 2 + x, height + y];
+  const targetRef = useRef<EventTarget>(null);
 
   const bind = useDrag(
     (state) => {
+      if(state.event.type === 'pointerdown') {
+        targetRef.current = state.event.currentTarget;
+      }
+
+      if(!state.intentional || !targetRef.current) {
+        return;
+      }
+
       if (state.first) {
         // @ts-ignore
-        const { x, bottom } = state.event.currentTarget.getBoundingClientRect();
+        const { x, bottom } = targetRef.current.getBoundingClientRect();
 
         // memo will hold the difference between the first point of impact and the origin
         const memo = [state.xy[0] - x - width / 2, state.xy[1] - bottom];
@@ -55,11 +65,16 @@ export const useNodeDrag = ({ x, y, height, width, onDrag, onDragEnd, onDragStar
       onDrag(state, initial, node);
 
       if (state.last) {
+        targetRef.current = null;
         onDragEnd(state, initial, node);
         document.body.classList.remove('dragging');
       }
     },
-    { enabled: !disabled }
+    {
+      enabled: !disabled,
+      triggerAllEvents: true,
+      threshold: 5
+    }
   );
 
   return bind;
