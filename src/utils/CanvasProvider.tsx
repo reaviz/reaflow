ea60ac74b1880ca8caa23a038b6f7a9e4ed25672
Austin <amcdaniel2@gmail.com>
2020-11-09 18:14:33 -0500

    extract drag logic

--- src/utils/CanvasProvider.tsx
+++ src/utils/CanvasProvider.tsx
@@ -1,23 +1,9 @@
-import React, { createContext, useContext, useState } from 'react';
-import { EdgeSections } from '../symbols/Edge';
+import React, { createContext, useContext } from 'react';
 import { NodeData, PortData } from '../types';
-import { DragEvent, NodeDragEvents, Position } from './useNodeDrag';
+import { CanvasDragProps, useCanvasDrag } from './useCanvasDrag';
 
-export interface CanvasProviderValue extends NodeDragEvents {
-  dragCoords: EdgeSections[] | null;
-  canLinkNode: boolean | null;
-  dragNode: NodeData | null;
-  dragPort: PortData | null;
-  enteredNode: NodeData | null;
+export interface CanvasProviderValue extends CanvasDragProps {
   selections?: string[];
-  onEnter?: (
-    event: React.MouseEvent<SVGGElement, MouseEvent>,
-    data: NodeData | PortData
-  ) => void;
-  onLeave?: (
-    event: React.MouseEvent<SVGGElement, MouseEvent>,
-    data: NodeData | PortData
-  ) => void;
 }
 
 export const CanvasContext = createContext<CanvasProviderValue>({} as any);
@@ -37,79 +23,13 @@ export const CanvasProvider = ({
   onNodeLinkCheck,
   children
 }) => {
-  const [dragNode, setDragNode] = useState<NodeData | null>(null);
-  const [dragPort, setDragPort] = useState<PortData | null>(null);
-  const [enteredNode, setEnteredNode] = useState<NodeData | null>(null);
-  const [dragCoords, setDragCoords] = useState<EdgeSections[] | null>(null);
-  const [canLinkNode, setCanLinkNode] = useState<boolean | null>(null);
-
-  const onDragStart = (
-    _state: DragEvent,
-    _initial: Position,
-    node: NodeData,
-    port?: PortData
-  ) => {
-    setDragNode(node);
-    setDragPort(port);
-  };
-
-  const onDrag = (
-    { movement: [mx, my], memo: [ox, oy] }: DragEvent,
-    [ix, iy]: Position
-  ) => {
-    setDragCoords([
-      {
-        startPoint: { x: ix, y: iy },
-        endPoint: { x: ix + mx + ox, y: iy + my + oy }
-      }
-    ]);
-  };
-
-  const onDragEnd = () => {
-    if (dragNode && enteredNode && canLinkNode) {
-      onNodeLink(dragNode, enteredNode, dragPort);
-    }
-
-    setDragNode(null);
-    setDragPort(null);
-    setEnteredNode(null);
-    setDragCoords(null);
-  };
-
-  const onEnter = (
-    _event: React.MouseEvent<SVGGElement, MouseEvent>,
-    node: NodeData
-  ) => {
-    setEnteredNode(node);
-
-    if (dragNode && node) {
-      const canLink = onNodeLinkCheck(dragNode, node, dragPort);
-      const result =
-        (canLink === undefined || canLink) && dragNode.parent === node.parent;
-
-      setCanLinkNode(result);
-    }
-  };
-
-  const onLeave = () => {
-    setEnteredNode(null);
-    setCanLinkNode(null);
-  };
+  const dragProps = useCanvasDrag({ onNodeLink, onNodeLinkCheck });
 
   return (
     <CanvasContext.Provider
       value={{
         selections,
-        dragCoords,
-        canLinkNode,
-        dragNode,
-        dragPort,
-        enteredNode,
-        onDragStart,
-        onDrag,
-        onDragEnd,
-        onEnter,
-        onLeave
+        ...dragProps
       }}
     >
       {children}
---
+++ src/utils/useCanvasDrag.ts
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { EdgeSections } from '../symbols/Edge';
+import { NodeData, PortData } from '../types';
+import { DragEvent, NodeDragEvents, Position } from './useNodeDrag';
+
+export interface CanvasDragProps extends NodeDragEvents {
+  dragCoords: EdgeSections[] | null;
+  canLinkNode: boolean | null;
+  dragNode: NodeData | null;
+  dragPort: PortData | null;
+  enteredNode: NodeData | null;
+  onEnter?: (
+    event: React.MouseEvent<SVGGElement, MouseEvent>,
+    data: NodeData | PortData
+  ) => void;
+  onLeave?: (
+    event: React.MouseEvent<SVGGElement, MouseEvent>,
+    data: NodeData | PortData
+  ) => void;
+}
+
+export const useCanvasDrag = ({
+  onNodeLink,
+  onNodeLinkCheck
+}): CanvasDragProps => {
+  const [dragNode, setDragNode] = useState<NodeData | null>(null);
+  const [dragPort, setDragPort] = useState<PortData | null>(null);
+  const [enteredNode, setEnteredNode] = useState<NodeData | null>(null);
+  const [dragCoords, setDragCoords] = useState<EdgeSections[] | null>(null);
+  const [canLinkNode, setCanLinkNode] = useState<boolean | null>(null);
+
+  const onDragStart = (
+    _state: DragEvent,
+    _initial: Position,
+    node: NodeData,
+    port?: PortData
+  ) => {
+    setDragNode(node);
+    setDragPort(port);
+  };
+
+  const onDrag = (
+    { movement: [mx, my], memo: [ox, oy] }: DragEvent,
+    [ix, iy]: Position
+  ) => {
+    setDragCoords([
+      {
+        startPoint: { x: ix, y: iy },
+        endPoint: { x: ix + mx + ox, y: iy + my + oy }
+      }
+    ]);
+  };
+
+  const onDragEnd = () => {
+    if (dragNode && enteredNode && canLinkNode) {
+      onNodeLink(dragNode, enteredNode, dragPort);
+    }
+
+    setDragNode(null);
+    setDragPort(null);
+    setEnteredNode(null);
+    setDragCoords(null);
+  };
+
+  const onEnter = (
+    _event: React.MouseEvent<SVGGElement, MouseEvent>,
+    node: NodeData
+  ) => {
+    setEnteredNode(node);
+
+    if (dragNode && node) {
+      const canLink = onNodeLinkCheck(dragNode, node, dragPort);
+      const result =
+        (canLink === undefined || canLink) && dragNode.parent === node.parent;
+
+      setCanLinkNode(result);
+    }
+  };
+
+  const onLeave = () => {
+    setEnteredNode(null);
+    setCanLinkNode(null);
+  };
+
+  return {
+    dragCoords,
+    canLinkNode,
+    dragNode,
+    dragPort,
+    enteredNode,
+    onDragStart,
+    onDrag,
+    onDragEnd,
+    onEnter,
+    onLeave
+  };
+};
