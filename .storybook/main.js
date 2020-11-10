e86eae4e0ca313725f3999bd0f6255e276dd501b
Austin <amcdaniel2@gmail.com>
2020-11-10 09:36:01 -0500

    attemp to fix sandbox

--- .storybook/main.js
+++ .storybook/main.js
@@ -54,7 +54,8 @@ module.exports = {
                 content: {
                   ...reaflowCodesandboxTemplatePackageJSON,
                   dependencies: {
-                    ...reaflowCodesandboxTemplatePackageJSON.dependencies
+                    ...reaflowCodesandboxTemplatePackageJSON.dependencies,
+                    'elkjs': 'latest'
                   }
                 }
               }
