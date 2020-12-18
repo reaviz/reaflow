89cedab08d9610b5e8e710ebecc153e7e5807bf0
Austin McDaniel <amcdaniel2@gmail.com>
2020-12-18 08:16:51 -0500

    update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,3 @@
-const { resolve } = require('path');
-const codesandbox = require('remark-codesandbox');
-const reaflowCodesandboxTemplatePackageJSON = require('../docs/tools/templates/reaflow-codesandbox-template/package.json');
-
 module.exports = {
   stories: [
     '../docs/**/*.stories.mdx',
@@ -13,52 +9,5 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-docs/preset',
     '@storybook/addon-storysource'
-  ],
-  webpackFinal: (config) => {
-    const mdxRule = config.module.rules.find((rule) =>
-      rule.test.test('.stories.mdx')
-    );
-
-    const {
-      options: { remarkPlugins }
-    } = mdxRule.use.find(
-      ({ loader }) => loader === require.resolve('@mdx-js/loader')
-    );
-
-    remarkPlugins.push([
-      codesandbox,
-      {
-        mode: 'iframe',
-        query: {
-          fontsize: 14
-        },
-        customTemplates: {
-          reaflow: {
-            extends: `file:${resolve(
-              __dirname,
-              '../docs/tools/templates/reaflow-codesandbox-template'
-            )}`,
-            entry: 'src/App.js'
-          },
-          'reaflow-map': {
-            extends: 'reaflow',
-            files: {
-              'package.json': {
-                content: {
-                  ...reaflowCodesandboxTemplatePackageJSON,
-                  dependencies: {
-                    ...reaflowCodesandboxTemplatePackageJSON.dependencies,
-                    'elkjs': 'latest'
-                  }
-                }
-              }
-            }
-          }
-        },
-        autoDeploy: true
-      }
-    ]);
-
-    return config;
-  }
+  ]
 };
