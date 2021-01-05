7bb4f52ac7213a742c8c96a092c4ff34ac436fc0
Austin McDaniel <amcdaniel2@gmail.com>
2021-01-05 08:45:04 -0500

    prox dist cleanup

--- src/helpers/useProximity.ts
+++ src/helpers/useProximity.ts
@@ -33,63 +33,65 @@ export const useProximity = ({
     setMatrix(getCoords(canvasRef.current));
   }, [canvasRef]);
 
-  const onDrag = useCallback((event: PointerEvent) => {
-    if (!matrix) {
-      return;
-    }
-
-    const cubes = [];
-    const layout = canvasRef.current.layout;
-    const mousePoint = new Point2D(event.x, event.y).transform(matrix);
-
-    for (const node of layout.children) {
-      const points = [
-        // top-left
-        new Point2D(node.x, node.y),
-        // top-right
-        new Point2D(node.x + node.width, node.y),
-        // bottom-left
-        new Point2D(node.x, node.y + node.height),
-        // bottom-right
-        new Point2D(node.x + node.width, node.y + node.height)
-      ];
-
-      // The final distance we will use to eval
-      let dist;
-
-      // TODO: Make this support other shape types...
-      const intersects = IntersectionQuery
-        .pointInRectangle(mousePoint, points[0], points[3]);
-      if (intersects) {
-        dist = 0;
-      } else {
-        const dists: number[] = points.map(p => mousePoint.distanceFrom(p));
-        dist = Math.min(...dists);
+  const onDrag = useCallback(
+    (event: PointerEvent) => {
+      if (!matrix) {
+        return;
       }
 
-      cubes.push({
-        node,
-        dist
-      });
-    }
+      const cubes = [];
+      const layout = canvasRef.current.layout;
+      const mousePoint = new Point2D(event.x, event.y).transform(matrix);
 
-    let foundDist = minDistance;
-    let foundNodeId = null;
-    for (const cube of cubes) {
-      if (cube.dist < foundDist) {
-        foundNodeId = cube.node.id;
-        foundDist = cube.dist;
+      for (const node of layout.children) {
+        const points = [
+          // top-left
+          new Point2D(node.x, node.y),
+          // top-right
+          new Point2D(node.x + node.width, node.y),
+          // bottom-left
+          new Point2D(node.x, node.y + node.height),
+          // bottom-right
+          new Point2D(node.x + node.width, node.y + node.height)
+        ];
+
+        // TODO: Make this support other shape types...
+        const intersects = IntersectionQuery.pointInRectangle(
+          mousePoint,
+          points[0],
+          points[3]
+        );
+
+        // Calc the distances
+        const dists: number[] = points.map((p) => mousePoint.distanceFrom(p));
+        const minDist = Math.min(...dists);
+
+        cubes.push({
+          node,
+          minDist,
+          intersects
+        });
       }
-    }
 
-    if (foundNodeId !== lastMatchRef.current) {
-      onMatchChange?.(foundNodeId, foundDist);
-    }
+      let foundDist = minDistance;
+      let foundNodeId = null;
+      for (const cube of cubes) {
+        if (cube.minDist < foundDist) {
+          foundNodeId = cube.node.id;
+          foundDist = cube.minDist;
+        }
+      }
 
-    lastMatchRef.current = foundNodeId;
-    setMatch(foundNodeId);
-    setDistance(foundDist !== minDistance ? foundDist : null);
-  }, [matrix, canvasRef, minDistance, onMatchChange]);
+      if (foundNodeId !== lastMatchRef.current) {
+        onMatchChange?.(foundNodeId, foundDist);
+      }
+
+      lastMatchRef.current = foundNodeId;
+      setMatch(foundNodeId);
+      setDistance(foundDist !== minDistance ? foundDist : null);
+    },
+    [matrix, canvasRef, minDistance, onMatchChange]
+  );
 
   const onDragEnd = useCallback(() => {
     setMatch(null);
