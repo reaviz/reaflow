4baa285abc0e1e14df040911997bffc9459658be
Austin McDaniel <amcdaniel2@gmail.com>
2021-07-22 10:42:13 -0400

    add upsert example

--- stories/Drag.stories.tsx
+++ stories/Drag.stories.tsx
@@ -218,6 +218,105 @@ export const NodeRearranging = () => {
                 enteredNode &&
                 !hasLink(edges, enteredNode, node)
               ) {
+                const newEdges = edges.filter(e => e.to !== node.id);
+
+                setEdges([
+                  ...newEdges,
+                  createEdgeFromNodes(enteredNode, node)
+                ]);
+              }
+            }}
+          />
+        }
+        dragNode={<Node />}
+        dragEdge={null}
+        onCanvasClick={event => console.log('Canvas Clicked', event)}
+        onMouseEnter={() => setDroppable(true)}
+        onMouseLeave={() => setDroppable(false)}
+        onLayoutChange={layout => console.log('Layout', layout)}
+      />
+    </div>
+  );
+};
+
+export const NodeRearrangingUpsert = () => {
+  const [nodes, setNodes] = useState<NodeData[]>([
+    {
+      id: '1',
+      text: '1'
+    },
+    {
+      id: '2',
+      text: '2'
+    },
+    {
+      id: '3',
+      text: '3'
+    },
+    {
+      id: '4',
+      text: '4'
+    },
+    {
+      id: '5',
+      text: '5'
+    },
+    {
+      id: '6',
+      text: '6'
+    }
+  ]);
+
+  const [edges, setEdges] = useState<EdgeData[]>([
+    {
+      id: '1-2',
+      from: '1',
+      to: '2'
+    },
+    {
+      id: '2-3',
+      from: '2',
+      to: '3'
+    },
+    {
+      id: '2-4',
+      from: '2',
+      to: '4'
+    },
+    {
+      id: '2-5',
+      from: '2',
+      to: '5'
+    },
+    {
+      id: '5-6',
+      from: '5',
+      to: '6'
+    }
+  ]);
+
+  const [droppable, setDroppable] = useState<boolean>(false);
+  const [enteredNode, setEnteredNode] = useState<NodeData | null>(null);
+
+  return (
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+      <Canvas
+        nodes={nodes}
+        edges={edges}
+        node={
+          <Node
+            dragType="all"
+            onEnter={(_event, node) => setEnteredNode(node)}
+            onLeave={() => setEnteredNode(null)}
+            onDragEnd={(_event, _coords, node) => {
+              console.log('Entered Node:', enteredNode);
+              console.log('Node to Move:', node);
+
+              if (
+                droppable &&
+                enteredNode &&
+                !hasLink(edges, enteredNode, node)
+              ) {
                 const result = removeAndUpsertNodes(
                   nodes,
                   edges,
