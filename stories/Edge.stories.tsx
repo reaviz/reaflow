10d5b909eb6d009b43596d7b082356dca39da612
Joshua Weinstein <jweinstein@flashpoint-intel.com>
2021-10-25 11:48:09 -0400

    add Edge stories

--- stories/Edge.stories.tsx
+++ stories/Edge.stories.tsx
@@ -67,6 +67,90 @@ export const Adding = () => {
   );
 };
 
+export const NotUpsertable = () => {
+  const [nodes, setNodes] = useState<NodeData[]>([
+    {
+      id: '1',
+      text: 'Node 1'
+    },
+    {
+      id: '2',
+      text: 'Node 2'
+    }
+  ]);
+
+  const [edges, setEdges] = useState<EdgeData[]>([
+    {
+      id: '1-2',
+      from: '1',
+      to: '2'
+    }
+  ]);
+
+  return (
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+      <Canvas
+        nodes={nodes}
+        edges={edges}
+        edge={
+          <Edge
+            add={<Add hidden={false} />}
+            upsertable={false}
+            onAdd={(event, edge) => {
+              const id = `node-${Math.random()}`;
+              const newNode = {
+                id,
+                text: id
+              };
+
+              const results = upsertNode(nodes, edges, edge, newNode);
+              setNodes(results.nodes);
+              setEdges(results.edges);
+            }}
+          />
+        }
+        onLayoutChange={layout => console.log('Layout', layout)}
+      />
+    </div>
+  );
+};
+
+export const NotSelectable = () => {
+  const [nodes] = useState<NodeData[]>([
+    {
+      id: '1',
+      text: 'Node 1'
+    },
+    {
+      id: '2',
+      text: 'Node 2'
+    }
+  ]);
+
+  const [edges] = useState<EdgeData[]>([
+    {
+      id: '1-2',
+      from: '1',
+      to: '2'
+    }
+  ]);
+
+  return (
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+      <Canvas
+        nodes={nodes}
+        edges={edges}
+        edge={
+          <Edge
+            selectable={false}
+          />
+        }
+        onLayoutChange={layout => console.log('Layout', layout)}
+      />
+    </div>
+  );
+};
+
 export const Removeable = () => {
   const [selections, setSelections] = useState<string[]>(['1', '1-2']);
   const [nodes, setNodes] = useState<NodeData[]>([
@@ -119,6 +203,81 @@ export const Removeable = () => {
         }
         edge={
           <Edge
+            onClick={(event, edge) => {
+              console.log('Selecting Edge', event, edge);
+              setSelections([edge.id]);
+            }}
+            onRemove={(event, edge) => {
+              console.log('Removing Edge', event, edge);
+              setEdges(edges.filter(e => e.id !== edge.id));
+              setSelections([]);
+            }}
+          />
+        }
+        onCanvasClick={(event) => {
+          console.log('Canvas Clicked', event);
+          setSelections([]);
+        }}
+        onLayoutChange={layout => console.log('Layout', layout)}
+      />
+    </div>
+  );
+};
+
+export const NotRemoveable = () => {
+  const [selections, setSelections] = useState<string[]>(['1-2']);
+  const [nodes, setNodes] = useState<NodeData[]>([
+    {
+      id: '1',
+      text: 'Node 1'
+    },
+    {
+      id: '2',
+      text: 'Node 2'
+    },
+    {
+      id: '3',
+      text: 'Node 3'
+    }
+  ]);
+  const [edges, setEdges] = useState<EdgeData[]>([
+    {
+      id: '1-2',
+      from: '1',
+      to: '2'
+    },
+    {
+      id: '2-3',
+      from: '2',
+      to: '3'
+    }
+  ]);
+
+  return (
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+      <Canvas
+        nodes={nodes}
+        edges={edges}
+        selections={selections}
+        node={
+          <Node
+            removable={false}
+            onClick={(event, node) => {
+              console.log('Selecting Node', event, node);
+              setSelections([node.id]);
+            }}
+            onRemove={(event, node) => {
+              console.log('Removing Node', event, node);
+              const result = removeAndUpsertNodes(nodes, edges, node);
+              setEdges(result.edges);
+              setNodes(result.nodes);
+              setSelections([]);
+            }}
+          />
+        }
+        edge={
+          <Edge
+            removable={false}
             onClick={(event, edge) => {
               console.log('Selecting Edge', event, edge);
               setSelections([edge.id]);
