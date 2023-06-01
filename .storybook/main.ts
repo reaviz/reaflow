de19ccd37c4638d8f3153d6b93b85dcfc99d98c2
Evgeniy <evgeniy.kulinich@mev.com>
2023-06-01 12:11:20 +0300

    Update Storybook version

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: [
+    '../docs/**/*.mdx',
+    '../src/**/*.stories.tsx',
+    '../stories/**/*.stories.tsx'
+  ],
+  addons: [
+    '@storybook/addon-storysource',
+    '@storybook/addon-essentials',
+
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  }
+};
+
+export default config;
