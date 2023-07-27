ef67f66cd075ed386ef763a6d8ec5646db9728e8
amcdnl <amcdaniel2@gmail.com>
2023-07-27 08:51:51 -0400

    fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'prettier',
-    'prettier/react',
     'plugin:react-hooks/recommended',
     'plugin:storybook/recommended'
   ],
