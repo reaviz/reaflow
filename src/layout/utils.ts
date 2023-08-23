648a2f26dbc04523d14412c2491b2fd2e349641a
AykutSarac <aykutsarac0@gmail.com>
2023-08-23 09:07:24 +0300

    fix: calculateSize fn

--- src/layout/utils.ts
+++ src/layout/utils.ts
@@ -12,9 +12,9 @@ export function measureText(text: string) {
   let result = { height: 0, width: 0 };
 
   if (text) {
-    // @ts-ignore
     // Reference: https://github.com/reaviz/reaflow/pull/229
-    const fn = calculateSize || calculateSize.default;
+    // @ts-ignore
+    const fn = typeof calculateSize === 'function' ? calculateSize : calculateSize.default;
     result = fn(text, {
       font: 'Arial, sans-serif',
       fontSize: '14px'
